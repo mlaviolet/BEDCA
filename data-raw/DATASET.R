5fb10f6c2b0103d7544b5c99f4419f5e2c356679
Michael Laviolette <statman89@outlook.com>
2025-09-17 21:02:26 -0400

    Remove all objects from workspace

--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -1142,12 +1142,13 @@ for(i in 1:length(ls())) {
   cat("Saved", ls()[i], "\n")
   }
 
-rm(i)
 file.remove(here::here("data", "i.Rdata"))
 
 # for some reason these two weren't saved; save manually
 save("table7_19", file = file.path(here::here("data/table7_19.RData")))
 save("table9_02", file = file.path(here::here("data/table9_02.RData")))
+
+rm(list = ls())
 
 # search "r package error data-raw/na does not exist"
 # Error : 'C:/Users/Admin/AppData/Local/Temp/RtmpMj2J9K/R.INSTALL32e450f44896/BEDCA/data-raw/NA' does not exist.
