9feca64833b34d41535e22260364a88aae63972e
Michael Laviolette <statman89@outlook.com>
2025-09-18 02:05:21 -0400

    Initial commit

---
+++ R/BEDCA-package.R
@@ -0,0 +1,49 @@
+#' @keywords internal
+"_PACKAGE"
+
+## usethis namespace: start
+## usethis namespace: end
+
+#' @name ch1_eoc_prob4
+#' @docType data
+#' @title Chapter 1, Exercise 4
+#' @usage data(ch1_eoc_prob4)
+#' @keywords datasets
+#'
+#' @format A data frame with 8 rows and 3 variables
+#'  \describe{
+#'   \item{Bushing}{Spanner bushing measured}
+#'   \item{A}{Measurements by student technician A (inches)}
+#'   \item{B}{Measurements by student technician B (inches)}
+#'   }
+#'
+#' @source Vardeman SB & Jobe MJ (2023) Basic Engineering Data Collection and Analysis
+#' \url{https://www.iastatedigitalpress.com/plugins/books/127/}
+#'
+#' @examples
+#' data(ch1_eoc_prob4)
+#' str(ch1_eoc_prob4)
+
+NULL
+
+#' @name ch1_xmp07
+#' @docType data
+#' @title Chapter 1, Example 7
+#' @usage data(ch1_xmp07)
+#' @keywords datasets
+#'
+#' @format A data frame with 10 rows and 2 variables
+#'  \describe{
+#'   \item{tech}{Student who made measurement}
+#'   \item{thickness}{Thickness of book paper (mm)}
+#'   }
+#'
+#' @source Vardeman SB & Jobe MJ (2023) Basic Engineering Data Collection and Analysis
+#' \url{https://www.iastatedigitalpress.com/plugins/books/127/}
+#'
+#' @examples
+#' data(ch1_xmp07)
+#' str(ch1_xmp07)
+#'
+
+NULL
